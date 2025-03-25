9e5f3898f85f9688b751070a71dc36771d2fd5ef
shankar bandi <shankarbandi72@gmail.com>
2025-03-25 09:30:13 +0530

    Update ProductCard.js

--- src/ProductCard/ProductCard.js
+++ src/ProductCard/ProductCard.js
@@ -1,23 +1,3 @@
-// import React from 'react';
-// // import './ProductCard.css';
-// // import './ProductCard/ProductCard.css';
-// import './ProductCard.css';
-
-// const ProductCard = ({ product }) => {
-//     return (
-//         <div className="product-card">
-//             <img src={product.image} alt={product.title} />
-//             <h3>{product.title}</h3>
-//             <p>${product.price}</p>
-//             <button>Add to Cart</button>
-//         </div>
-//     );
-// };
-
-// export default ProductCard;
-
-
-
 import React, { useContext } from 'react';
 import { CartContext } from '../CartContext';
 // import { CartContext } from '../CartContext';
