ca2eaadff0f0e8fbc5406214403e755a2798e080
shankar bandi <shankarbandi72@gmail.com>
2025-03-25 09:25:28 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import './Contact.css';
-
-// const Contact = () => {
-//     return (
-//         <div className="contact">
-//             <h2>Contact Us</h2>
-//             <form>
-//                 <label htmlFor="name">Name</label>
-//                 <input type="text" id="name" placeholder="Your Name" required />
-
-//                 <label htmlFor="email">Email</label>
-//                 <input type="email" id="email" placeholder="Your Email" required />
-
-//                 <label htmlFor="message">Message</label>
-//                 <textarea id="message" placeholder="Your Message" required></textarea>
-
-//                 <button type="submit">Send</button>
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default Contact;   
-
-
-
-
 
 import React, { useState } from 'react';
 import './Contact.css';
