d511b3f7890e3779b04c12dc9b4410f4ecf12fa2
shankarshankar12 <shankarbandi72@gmail.com>
2025-03-26 09:58:34 +0530

    Add local storage to CartContext

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,9 +1,18 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState,useEffect } from 'react';
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+    // const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        const savedCart = localStorage.getItem('cart');
+        return savedCart ? JSON.parse(savedCart) : [];
+    });
+
+    // Save cart to local storage whenever it changes
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
 
     // Add item to cart
     const addToCart = (product) => {
